e80d42b7d8b9c9235642feeb1532d93dd1f09368
B21098 <e1b21098@oit.ac.jp>
2023-10-17 11:43:36 +0900

    じゃんけんの正常化

--- src/main/java/oit/is/z1819/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z1819/kaizi/janken/controller/JankenController.java
@@ -6,25 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import oit.is.z1819.kaizi.janken.model.Janken;
 import oit.is.z1819.kaizi.janken.model.Entry;
 
-@RequestMapping("/janken")
 @Controller
 public class JankenController {
 
   @Autowired
   private Entry entry;
 
-  @GetMapping
+  @GetMapping("/janken")
   public String janken(ModelMap model, Principal prin) {
     String loginUser = prin.getName(); // ログインユーザ情報
     this.entry.addUser(loginUser);
-    model.addAttribute("login_user", loginUser);
-    model.addAttribute("entry", this.entry);
+    model.addAttribute("loginUser", loginUser);
+    model.addAttribute("users", this.entry.getUsers());
     return "janken.html";
   }
 
@@ -32,13 +30,12 @@ public class JankenController {
   public String jankengame(@RequestParam String hand, ModelMap model, Principal prin) {
     Janken janken = new Janken();
     janken.setPlayer(hand);
+    janken.setCpu();
 
-    String result = janken.determineWinner();
     model.addAttribute("users", this.entry.getUsers());
-    model.addAttribute("num_users", this.entry.getNumUsers());
     model.addAttribute("player_hand", janken.getPlayer());
     model.addAttribute("cpu_hand", janken.getCpu());
-    model.addAttribute("result",result);
+    model.addAttribute("result",janken.jankenResult());
 
     return "janken.html";
   }
