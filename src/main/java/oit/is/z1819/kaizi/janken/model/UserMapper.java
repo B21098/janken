3c5a1198a2020ccf176027cc6355a1930d2cca39
B21098 <e1b21098@oit.ac.jp>
2023-10-26 18:21:05 +0900

    相手のidを取得し試合ページへ移動する

--- src/main/java/oit/is/z1819/kaizi/janken/model/UserMapper.java
+++ src/main/java/oit/is/z1819/kaizi/janken/model/UserMapper.java
@@ -7,13 +7,13 @@ import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
-    /*
-     * @Select("SELECT * from users where id = #{id}")
-     * User selectById(String id);
-     * 
-     * @Select("SELECT * from users where name = #{name}")
-     * User selectByName(String name);
-     */
+
+    @Select("SELECT * from users where id = #{id}")
+    User selectById(String id);
+
+    @Select("SELECT * from users where name = #{name}")
+    User selectByName(String name);
+
     @Select("SELECT * from users")
     ArrayList<User> selectAllUsers();
 }
